76152e38cc2bba6da38f465fef6bd88ab770b5e2
MuhamedAhmed112 <ma0758746@gmail.com>
2025-04-20 16:30:12 +0200

    solve some errors

--- GP/MedicalgraduationProject/src/app/layout/additions/reset-pass/reset-pass.component.ts
+++ GP/MedicalgraduationProject/src/app/layout/additions/reset-pass/reset-pass.component.ts
@@ -9,46 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ResetPassComponent  {
   
-    // private inputs: HTMLElement | null;
-
-    // constructor() {
-    //     this.inputs = document.getElementById("inputs");
-    //     if (this.inputs) {
-    //         this.initEvents();
-    //     }
-    // }
-
-    // private initEvents(): void {
-    //     this.inputs!.addEventListener("input", (e: Event) => {
-    //         const target = e.target as HTMLInputElement;
-    //         const val = target.value;
-
-    //         if (isNaN(Number(val))) {
-    //             target.value = "";
-    //             return;
-    //         }
-
-    //         if (val !== "") {
-    //             const next = target.nextElementSibling as HTMLInputElement | null;
-    //             if (next && next.focus) {
-    //                 next.focus();
-    //             }
-    //         }
-    //     });
-
-    //     this.inputs!.addEventListener("keyup", (e: KeyboardEvent) => {
-    //         const target = e.target as HTMLInputElement;
-    //         const key = e.key.toLowerCase();
-
-    //         if (key === "backspace" || key === "delete") {
-    //             target.value = "";
-    //             const prev = target.previousElementSibling as HTMLInputElement | null;
-    //             if (prev && prev.focus) {
-    //                 prev.focus();
-    //             }
-    //         }
-    //     });
-    // }
+     
 }
 
 
